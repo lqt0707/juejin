27a543e776627189211d00c73bd13a7c48394d42
刘秋涛 <qiutao.liu@7x-networks.com>
2023-11-23 17:42:27 +0800

    feat(ssg): cmd

--- island-ssg/src/node/cli.ts
+++ island-ssg/src/node/cli.ts
@@ -1,4 +1,7 @@
 import cac from "cac";
+import { createDevServer } from "./dev";
+import { build } from "./build";
+const path = require("path");
 
 const version = require("../../package.json").version;
 
@@ -9,12 +12,24 @@ cli
   .alias("dev")
   .action(async (root: string) => {
     console.log("dev", root);
+
+    root = root ? path.resolve(root) : process.cwd();
+    const server = await createDevServer(root);
+    await server.listen();
+    server.printUrls();
   });
 
 cli
   .command("build [root]", "build for production")
   .action(async (root: string) => {
     console.log("build", root);
+
+    try {
+      root = path.resolve(root);
+      await build(root);
+    } catch (error) {
+      console.log(error);
+    }
   });
 
 cli.parse();
