c1ba4b357f416709997c8f63c6b999f20c05a9ac
刘秋涛 <qiutao.liu@7x-networks.com>
2023-11-29 18:06:41 +0800

    feat(ssg): home-page

--- island-ssg/src/theme-default/Layout/index.tsx
+++ island-ssg/src/theme-default/Layout/index.tsx
@@ -1,13 +1,28 @@
-// src/theme-default/Layout/index.tsx
+import { usePageData } from '../../runtime';
 import { Nav } from '../components/Nav';
 import '../styles/base.css';
 import '../styles/vars.css';
 import 'uno.css';
+import { HomeLayout } from './HomeLayout/index';
 
 export function Layout() {
+  const pageData = usePageData();
+  const { pageType } = pageData;
+  console.log('pageType----', pageType);
+
+  const getContent = () => {
+    if (pageType === 'home') {
+      return <HomeLayout />;
+    } else if (pageType === 'doc') {
+      return <div>正文页面</div>;
+    } else {
+      return <div>404 页面</div>;
+    }
+  };
   return (
     <div>
       <Nav />
+      {getContent()}
     </div>
   );
 }
