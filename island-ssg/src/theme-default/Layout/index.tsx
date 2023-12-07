967a2acfdac90c6509beba6aa2e37d4b8c0871ff
刘秋涛 <qiutao.liu@7x-networks.com>
2023-12-07 16:46:13 +0800

    feat(ssg): preview-dev

--- island-ssg/src/theme-default/Layout/index.tsx
+++ island-ssg/src/theme-default/Layout/index.tsx
@@ -7,6 +7,7 @@ import 'uno.css';
 import { HomeLayout } from './HomeLayout/index';
 import { DocLayout } from './DocLayout';
 import { Helmet } from 'react-helmet-async';
+import { NotFoundLayout } from './NotFoundLayout';
 
 export function Layout() {
   const pageData = usePageData();
@@ -18,7 +19,8 @@ export function Layout() {
     } else if (pageType === 'doc') {
       return <DocLayout />;
     } else {
-      return <div>404 页面</div>;
+      // 接入 404 页面
+      return <NotFoundLayout />;
     }
   };
   return (
