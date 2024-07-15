3f18a1e3d4e3c823708dbfc33e904de8a5269838
qiutao.liu <qiutao.liu@7x-networks.com>
2024-07-15 15:08:34 +0800

    feat(state): build zustand

--- state-management-collection/packages/zustand/src/shallow.ts
+++ state-management-collection/packages/zustand/src/shallow.ts
@@ -1,31 +1,30 @@
 export function shallow<T>(objA: T, objB: T) {
   if (Object.is(objA, objB)) {
-    return true;
+    return true
   }
   if (
-    typeof objA !== "object" ||
+    typeof objA !== 'object' ||
     objA === null ||
-    typeof objB !== "object" ||
+    typeof objB !== 'object' ||
     objB === null
   ) {
-    return false;
+    return false
   }
 
-  const keysA = Object.keys(objA);
-  const keysB = Object.keys(objB);
+  const keysA = Object.keys(objA)
+  const keysB = Object.keys(objB)
 
   if (keysA.length !== keysB.length) {
-    return false;
+    return false
   }
 
   for (let i = 0; i < keysA.length; i++) {
     if (
       !Object.prototype.hasOwnProperty.call(objB, keysA[i]) ||
-      !Object.is(objA[keysA[i]], objB[keysA[i]])
+      !Object.is(objA[keysA[i] as keyof T], objB[keysA[i] as keyof T])
     ) {
-      return false;
+      return false
     }
   }
-
-  return true;
+  return true
 }
--- state-management-collection/rollup.config.js
+++ state-management-collection/rollup.config.js
@@ -1,18 +1,20 @@
-const createBabelConfig = require("./babel.config.js");
-const resolve = require("@rollup/plugin-node-resolve");
-const babelPlugin = require("@rollup/plugin-babel");
-const commonjs = require("@rollup/plugin-commonjs");
-const { dts } = require("rollup-plugin-dts");
+const createBabelConfig = require('./babel.config.js')
+const resolve = require('@rollup/plugin-node-resolve')
+const babelPlugin = require('@rollup/plugin-babel')
+const commonjs = require('@rollup/plugin-commonjs')
+const { dts } = require('rollup-plugin-dts')
+const banner2 = require('rollup-plugin-banner2')
 
-const extensions = [".ts", ".tsx"];
+const extensions = ['.ts', '.tsx']
+const cscComment = `'use client';\n`
 
 function getBabelOptions() {
   return {
     ...createBabelConfig,
     extensions,
-    babelHelpers: "bundled",
+    babelHelpers: 'bundled',
     comments: false,
-  };
+  }
 }
 
 function createDeclarationConfig(input, output) {
@@ -20,62 +22,61 @@ function createDeclarationConfig(input, output) {
     input,
     output: {
       file: output,
-      format: "es",
+      format: 'es',
     },
     plugins: [dts()],
-  };
+  }
 }
 
 function createESMConfig(input, output) {
   return {
     input,
-    output: { file: output, format: "esm" },
+    output: { file: output, format: 'esm' },
     plugins: [
       resolve({ extensions }),
       commonjs(),
       babelPlugin(getBabelOptions()),
+      banner2(() => cscComment),
     ],
-  };
+  }
 }
 
 function createCommonJSConfig(input, output) {
   return {
     input,
-    output: { file: output, format: "cjs" },
+    output: { file: output, format: 'cjs' },
     plugins: [
       resolve({ extensions }),
       commonjs(),
       babelPlugin(getBabelOptions()),
+      banner2(() => cscComment),
     ],
-  };
+  }
 }
 
 function createUMDConfig(input, output, name) {
   return {
     input,
-    output: { file: output, format: "umd", name },
+    output: { file: output, format: 'umd', name },
     plugins: [
       resolve({ extensions }),
       commonjs(),
       babelPlugin(getBabelOptions()),
+      banner2(() => cscComment),
     ],
-  };
+  }
 }
 
 module.exports = (args) => {
-  const packageName = args.package;
+  const packageName = args.package
 
-  const input = `packages/${packageName}`;
-  const output = `packages/${packageName}/dist`;
+  const input = `packages/${packageName}/src/index.ts`
+  const output = `packages/${packageName}/dist`
 
   return [
-    createDeclarationConfig(input, output),
-    createESMConfig(`${input}/src/index.ts`, `${output}/index.mjs`),
-    createCommonJSConfig(`${input}/src/index.ts`, `${output}/index.cjs.js`),
-    createUMDConfig(
-      `${input}/src/index.ts`,
-      `${output}/index.umd.js`,
-      packageName
-    ),
-  ];
-};
+    createDeclarationConfig(input, `${output}/index.d.ts`),
+    createESMConfig(input, `${output}/index.mjs`),
+    createCommonJSConfig(input, `${output}/index.cjs`),
+    createUMDConfig(input, `${output}/index.umd.js`, packageName),
+  ]
+}
