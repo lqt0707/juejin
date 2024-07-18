fd48070d66e39a7b37124954a2e8032b8147bcc2
qiutao.liu <qiutao.liu@7x-networks.com>
2024-07-18 11:43:00 +0800

    feat(state): jotai devtools

---
+++ state-management-collection/packages/jotai/src/devtools.ts
@@ -0,0 +1,24 @@
+import { Store, useStore } from './store'
+import { ReadableAtom } from './atom'
+import { useDebugValue, useEffect, useState } from 'react'
+
+const stateToPrintable = ([store, atoms]: [Store, ReadableAtom<unknown>[]]) =>
+  atoms.reduce(
+    (res, atom) => {
+      const atomState = store.get(atom)
+      res[atom.debugLabel] = { value: atomState }
+      return res
+    },
+    {} as Record<string, unknown>,
+  )
+
+export function useAtomsDebugValue() {
+  const store = useStore()
+  const [atoms, setAtoms] = useState<ReadableAtom<unknown>[]>([])
+
+  useEffect(() => {
+    setAtoms(Array.from(store.get_mounted_atoms()))
+  }, [store])
+
+  useDebugValue([store, atoms], stateToPrintable)
+}
